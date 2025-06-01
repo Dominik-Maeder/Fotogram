64ea5283b5a83a3f7e5faa12ffebaa03c8f109ee
LanceDark <dominik.maeder.97@web.de>
2025-06-01 20:15:36 +0200

    Added: Various Functions to show overlay

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
 let img = [
     {
-        src:'./assets/img/1.jpg',
+        src:'./assets/img/img11.jpg',
         title: 'Test',
         alt: 'Test',
     },
     {
-        src:'/assets/img/5.jpg',
+        src:'/assets/img/img5.jpg',
         title: 'Test',
         alt: 'Test',
     },
     {
-        src:'./assets/img/7.jpg',
+        src:'./assets/img/img7.jpg',
         title: 'Test',
         alt: 'Test',
     },
 
     {
-        src:'./assets/img/8.jpg',
+        src:'./assets/img/img8.jpg',
         title: 'Test',
         alt: 'Test',
     },
     {
-        src:'./assets/img/9.jpg',
+        src:'./assets/img/img9.jpg',
         title: '',
         alt: '',
     },
     {
-        src:'./assets/img/11.jpg',
+        src:'./assets/img/img11.jpg',
         title: '',
         alt: '',
     },
     {
-        src:'./assets/img/12.jpg',
+        src:'./assets/img/img12.jpg',
         title: '',
         alt: '',
     },
@@ -48,9 +48,28 @@ function init(){
 
 function getImgTemplate(i){
     return `
-    <div>
-        <img src="${img[i].src}" alt="${img[i].alt}"></img>
+    <div class="img-container" onclick="openOverlay(event, ${i})">
+        <img src="${img[i].src}" alt="${img[i].alt}">
         <p>${img[i].title}</p>
      </div>
+    `
+}
+
+function openOverlay(e, i){
+    e.preventDefault();
+    let content  = document.getElementById('main');
+
+    let overlay = document.createElement('div');
+    overlay.classList.add('overlay')
+    overlay.innerHTML = renderOverlay(i)
+    content.appendChild(overlay);
+}
+
+function renderOverlay(i){
+    return `
+    <div class="overlay-content">
+        <img src="${img[i].src}" alt="${img[i].alt}">
+        <p>${img[i].title}</p>      
+    </div>
     `
 }
